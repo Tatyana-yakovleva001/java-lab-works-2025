9e90d43bad0ff5bb2d67462a31063e057ce12c1b
Tatyana-yakovleva001 <owodpxls@gmail.com>
2025-12-10 14:57:44 +0300

    Lab1 done

---
+++ Lab1project/src/main/java/org/example/Assistent.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+/*
+public class Assistent {
+    public static void main(String[] args){
+        Scanner one = new Scanner(System.in);
+        System.out.println("Добавьте своё имя: ");
+        String name = one.nextLine();
+        Scanner two = new Scanner(System.in);
+        System.out.println("Какой сегодня день недели? ");
+        String WeekDay = two.nextLine();
+        Scanner three = new Scanner(System.in);
+        System.out.println("Какая температура за окном? ");
+        String weather = three.nextLine();
+
+        if(WeekDay.equals("Понедельник") && weather.equals())
+    }
+}*/
