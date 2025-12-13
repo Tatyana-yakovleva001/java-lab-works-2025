aec2bc2fd133c8dbbc7d82948abcb1980d07b68c
Tatyana-yakovleva001 <owodpxls@gmail.com>
2025-12-13 22:58:00 +0300

    2 задание

--- Lab1project/src/main/java/lab1task1.java
+++ Lab1project/src/main/java/lab1task1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class lab1 {
+public class lab1task1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите первый аргумент");
